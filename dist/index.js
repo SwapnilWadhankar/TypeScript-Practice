4fd90c8a3a42b2ca5eaad5647de3141d653b91a8
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2023-07-08 17:54:09 +0530

    This project is for learning TypeScript only

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("hello");
+console.log("This is some message");
+console.log("this is the new file");
+console.log("this is the new file");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("hello");
+console.log("This is some message");
+
+console.log("this is the new file");
+console.log("this is the new file")
