15d43c191c45b326d0cdc3a6a3052035cbeb217f
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2023-07-12 21:56:38 +0530

    Objects and Function creation TypeScript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-"use strict";
 function returnUser(someUser) {
-    console.log(`Hi this is ${someUser.name} of id ${someUser.id}`);
+    console.log("Hi this is ".concat(someUser.name, " of id ").concat(someUser.id));
     if (someUser.isPaid == true) {
         console.log("Fees paid");
     }
@@ -8,11 +7,10 @@ function returnUser(someUser) {
         console.log("Please pay the fee for this course.");
     }
 }
-let user1 = {
+var user1 = {
     name: "Swapnil",
     id: 1759229267,
     isPaid: true
 };
 returnUser(user1);
 console.log("This is the end");
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-let greetings:string="Hellow Swapnil";
-console.log(greetings);
-let smallerCase: String=greetings.toLowerCase();
-console.log(smallerCase);
-console.log("nothing");
+type user = {
+    name : String,
+    id: Number,
+    isPaid: Boolean
+}
+
+function returnUser(someUser: user){
+    console.log(`Hi this is ${someUser.name} of id ${someUser.id}`);
+    if (someUser.isPaid == true){
+        console.log("Fees paid");
+    }else{
+        console.log("Please pay the fee for this course.")
+    }
+}
+
+let user1 : user = {
+    name : "Swapnil",
+    id : 1759229267,
+    isPaid: true
+}
+
+returnUser(user1);
+console.log("This is the end")
